3f3a4d17519b833e8cbbf51a2b87ddb9d8409688
Michael <michael@passthekeys.co.uk>
2021-10-28 18:40:32 +0100

    remove font-awesome

--- src/Gallery/Loading.js
+++ src/Gallery/Loading.js
@@ -1,12 +1,10 @@
 import styles from './loading.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 function Loading() {
     return (
         <div className={styles.loading}>
             <p className={styles.spinner}>
-                <FontAwesomeIcon icon={faSpinner} spin />
+                <p>Loading...</p>
             </p>
         </div>
     )
